a184cb78001af7dca335fdaa49d61827dab27419
Vinod Jadhav <vinod.jadhav025@gmail.com>
2026-02-21 12:57:07 +0530

    Update addition and substraction title and message

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showMessage() {
-    document.getElementById("message").innerText = `Hey I am Swaransh!, Hi,
-I’m building something exciting — an Addition & Subtraction Machine.
-It’s under construction and getting smarter every day.
+    document.getElementById("message").innerText = `Hey I am Swaransh!,
+I'm building something exciting - an Addition & Subtraction Machine.
+It's under construction and getting smarter every day.
 Stay tuned for the launch! 🎉`;
 }
